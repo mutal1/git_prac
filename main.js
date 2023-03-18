43119c4cfc375738db8bdf3c341c45feac2e1db8
mutal <cksrb0710@naver.com>
2023-03-18 19:48:04 +0900

    test

---
+++ main.js
@@ -0,0 +1,10 @@
+const moreBtn = document.querySelector('.info .metadata .moreBtn');
+const title =  document.querySelector('.info .metadata .title');
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+});
+
+console.log(title);
+console.log(moreBtn);
